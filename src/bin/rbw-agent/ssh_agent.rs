8b30c2900d46a5d20a6c244c8ebcffa1a0f8bf17
Christoph Blüm <christoph.peter.bluem@gmail.com>
2026-01-29 22:24:55 +0100

    feat: don't truncate ssh key comments

--- src/bin/rbw-agent/ssh_agent.rs
+++ src/bin/rbw-agent/ssh_agent.rs
@@ -43,7 +43,7 @@ impl ssh_agent_lib::agent::Session for SshAgent {
                 p.parse::<ssh_agent_lib::ssh_key::PublicKey>()
                     .map(|pk| ssh_agent_lib::proto::Identity {
                         pubkey: pk.key_data().clone(),
-                        comment: String::new(),
+                        comment: p.splitn(3, ' ').nth(2).unwrap_or("").to_string(),
                     })
                     .map_err(ssh_agent_lib::error::AgentError::other)
             })
